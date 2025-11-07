c78422a02943f4e7db43b1e392337901a4d10004
sofia sayago <sofiasayago.ds@gmail.com>
2025-11-07 12:33:54 -0300

    Update CartWidget.jsx

---
+++ components/CartWidget.jsx
@@ -0,0 +1,10 @@
+function CartWidget() {
+  return (
+    <div className="d-flex align-items-center">
+      <i className="bi bi-cart-fill fs-4"></i>
+      <span className="badge bg-danger ms-1">3</span>
+    </div>
+  )
+}
+
+export default CartWidget
