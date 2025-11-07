da6d9f24739c49112a1a733e09da355fc6955d77
sofia sayago <sofiasayago.ds@gmail.com>
2025-11-07 12:35:02 -0300

    Create App component with NavBar and ItemListContainer

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ItemListContainer greeting="¡Bienvenida a SIBO Market, Sofía! " />
+    </>
+  )
+}
+
+export default App
